ccaf0c5dcfeda2407f543bb4228bb9f158539613
Jakub Cisło <cubix651@gmail.com>
2020-10-24 13:42:59 +0200

    Add CORS

--- ExpensesSplitter.WebApi/Startup.cs
+++ ExpensesSplitter.WebApi/Startup.cs
@@ -26,7 +26,7 @@ namespace ExpensesSplitter.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddCors();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -49,6 +49,7 @@ namespace ExpensesSplitter.WebApi
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseCors(builder => builder.AllowAnyOrigin());
 
             app.UseEndpoints(endpoints =>
             {
