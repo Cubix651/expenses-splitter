ccfb8767dad94351b10aa3ccf13dfa576d9ac015
Jakub Cisło <cubix651@gmail.com>
2020-11-01 20:08:27 +0100

    Create Settlement entity

---
+++ ExpensesSplitter.WebApi/Database/Models/Settlement.cs
@@ -0,0 +1,9 @@
+namespace ExpensesSplitter.WebApi.Database.Models
+{
+    public class Settlement
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
