19c1cefdf035e3cd32a5a42b568774297a1978e9
Jakub Cisło <cubix651@gmail.com>
2020-11-29 21:53:35 +0100

    Add migration for Expense Guid

---
+++ ExpensesSplitter.WebApi/Migrations/20201129205252_ExpenseGuid.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ExpensesSplitter.WebApi.Migrations
+{
+    public partial class ExpenseGuid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Expenses",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "Expenses",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(Guid));
+        }
+    }
+}
