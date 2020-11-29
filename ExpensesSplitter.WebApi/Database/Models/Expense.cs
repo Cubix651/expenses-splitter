f99a22a0ddd00a7191a9f614f1ec894e51264f76
Jakub Cisło <cubix651@gmail.com>
2020-11-29 21:51:31 +0100

    Change Expense id to Guid

--- ExpensesSplitter.WebApi/Database/Models/Expense.cs
+++ ExpensesSplitter.WebApi/Database/Models/Expense.cs
@@ -1,14 +1,19 @@
+using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ExpensesSplitter.WebApi.Database.Models
 {
     public class Expense
     {
-        public string Id { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         [Column(TypeName = "decimal(18,2)")]
         public decimal Amount {get; set; }
+        public string SettlementId {get; set;}
         public virtual Settlement Settlement { get; set; }
     }
 }
