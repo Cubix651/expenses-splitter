1cee636195c1dc97bf741644000d9a753ae9114b
Jakub Cisło <cubix651@gmail.com>
2020-11-30 21:09:53 +0100

    Show expenses in component

--- ExpensesSplitter.Frontend/src/app/expense-list/expense-list.component.ts
+++ ExpensesSplitter.Frontend/src/app/expense-list/expense-list.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ExpenseListService } from './expense-list.service';
+import { map, switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Expense } from './expense.model';
 
 @Component({
   selector: 'app-expense-list',
   templateUrl: './expense-list.component.html',
-  styleUrls: ['./expense-list.component.scss']
+  styleUrls: ['./expense-list.component.scss'],
+  providers: [ExpenseListService]
 })
 export class ExpenseListComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(
+    private readonly service: ExpenseListService,
+    private readonly route: ActivatedRoute,
+    ) { }
+    
+  expenses$: Observable<Expense[]>;
+  
   ngOnInit(): void {
+    this.expenses$ = this.route.params.pipe(
+      map(params => params['settlementId']),
+      switchMap(settlementId => this.service.getExpenses(settlementId))
+    );
   }
 
 }
