dfa443141240f333c335a4ea62031219ad93d054
Jakub Cisło <cubix651@gmail.com>
2020-12-05 22:16:30 +0100

    Add more methods to ExpensesService

--- ExpensesSplitter.Frontend/src/app/services/expenses.service.ts
+++ ExpensesSplitter.Frontend/src/app/services/expenses.service.ts
@@ -2,16 +2,33 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
-import { Expense } from '../models/expenses.model';
+import { Expense, NewExpense } from '../models/expenses.model';
 
 @Injectable()
 export class ExpensesService {
+  private readonly apiUrl = environment.apiUrl;
 
   constructor(
     private readonly http: HttpClient
   ) { }
 
   getExpenses(settlementId: string): Observable<Expense[]> {
-    return this.http.get<Expense[]>(`${environment.apiUrl}/settlements/${settlementId}/expenses`);
+    return this.http.get<Expense[]>(`${this.apiUrl}/settlements/${settlementId}/expenses`);
+  }
+
+  addExpense(settlementId: string, expense: NewExpense): Observable<string> {
+    return this.http.post<string>(`${this.apiUrl}/settlements/${settlementId}/expenses`, expense);
+  }
+
+  getExpense(settlementId: string, expenseId: string): Observable<Expense> {
+    return this.http.get<Expense>(`${this.apiUrl}/settlements/${settlementId}/expenses/${expenseId}`);
+  }
+
+  updateExpense(settlementId: string, expenseId: string, expense: NewExpense): Observable<any> {
+    return this.http.put<string>(`${this.apiUrl}/settlements/${settlementId}/expenses/${expenseId}`, expense);
+  }
+
+  deleteExpense(settlementId: string, expenseId: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/settlements/${settlementId}/expenses/${expenseId}`);
   }
 }
