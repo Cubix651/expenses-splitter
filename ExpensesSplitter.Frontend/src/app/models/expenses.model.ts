c4423e251901743daa5cc92445d8905a758aeafa
Jakub Cisło <cubix651@gmail.com>
2020-12-09 22:17:30 +0100

    Update expenses ng models

--- ExpensesSplitter.Frontend/src/app/models/expenses.model.ts
+++ ExpensesSplitter.Frontend/src/app/models/expenses.model.ts
@@ -1,9 +1,16 @@
+import { SettlementUser } from './settlement-user.model';
+
 export interface NewExpense {
     name: string;
     description: string;
     amount: number;
+    whoPaidId: string;
 }
 
-export interface Expense extends NewExpense {
+export interface Expense {
     id: string;
+    name: string;
+    description: string;
+    amount: number;
+    whoPaid: SettlementUser;
 }
