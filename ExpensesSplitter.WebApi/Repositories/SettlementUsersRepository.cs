ac0d3075e7f1c183573d8ed4844dfec57c5754eb
Jakub Cisło <cubix651@gmail.com>
2020-12-09 22:07:19 +0100

    Create SettlementUsersRepository

---
+++ ExpensesSplitter.WebApi/Repositories/SettlementUsersRepository.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ExpensesSplitter.WebApi.Database;
+using ExpensesSplitter.WebApi.Models;
+using AutoMapper;
+
+namespace ExpensesSplitter.WebApi.Repositories
+{
+    public interface ISettlementUsersRepository
+    {
+        IEnumerable<SettlementUser> GetSettlementUsers(string settlementId);
+        SettlementUser GetSettlementUser(Guid settlementUserId);
+    }
+
+    public class SettlementUsersRepository : ISettlementUsersRepository
+    {
+        private readonly ExpensesSplitterContext _context;
+        private readonly IMapper _mapper;
+
+        public SettlementUsersRepository(ExpensesSplitterContext context,
+            IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public IEnumerable<SettlementUser> GetSettlementUsers(string settlementId)
+        {
+            return _context.SettlementUsers
+                .Where(u => u.SettlementId == settlementId)
+                .Select(u => _mapper.Map<SettlementUser>(u));
+        }
+
+        public SettlementUser GetSettlementUser(Guid settlementUserId)
+        {
+            var user = _context.SettlementUsers
+                .First(u => u.Id == settlementUserId);
+            return _mapper.Map<SettlementUser>(user);
+        }
+    }
+}
