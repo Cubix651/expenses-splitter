2df85445470f676a84a3784672d7d2ed235ff1b4
Jakub Cisło <cubix651@gmail.com>
2020-12-30 17:25:35 +0100

    Create SettlementSolutionController

---
+++ ExpensesSplitter.WebApi/Controllers/SettlementSolutionController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ExpensesSplitter.WebApi.Providers;
+
+namespace ExpensesSplitter.WebApi.Controllers
+{
+    [Route("api/settlements/{settlementId}/solution")]
+    [ApiController]
+    public class SettlementSolutionController : ControllerBase
+    {
+        private readonly ISettlementSolutionProvider _settlementSolutionProvider;
+
+        public SettlementSolutionController(ISettlementSolutionProvider settlementSolutionProvider)
+        {
+            _settlementSolutionProvider = settlementSolutionProvider;
+        }
+
+        [HttpGet("")]
+        public ActionResult GetAll(string settlementId)
+        {
+            return Ok(_settlementSolutionProvider.GetSolutionTransactions(settlementId));
+        }
+    }
+}
