de56b0044fcaa622c2699250b1cd0e9f0b312ca0
Jakub Cisło <cubix651@gmail.com>
2020-12-31 20:26:36 +0100

    Create Transaction entity

---
+++ ExpensesSplitter.WebApi/Database/Models/Transaction.cs
@@ -0,0 +1,19 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpensesSplitter.WebApi.Database.Models
+{
+    public class Transaction
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public Guid Id { get; set; }
+        public virtual SettlementUser From { get; set; }
+        public Guid FromId { get; set; }
+        public virtual SettlementUser To { get; set; }
+        public Guid ToId { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Amount { get; set; }
+    }
+}
