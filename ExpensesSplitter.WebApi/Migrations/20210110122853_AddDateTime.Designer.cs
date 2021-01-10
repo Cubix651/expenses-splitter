79e211ebf60922fc13c277259bda3f051de6f9af
Jakub Cisło <cubix651@gmail.com>
2021-01-10 22:50:59 +0100

    Create migration AddDateTime

--- ExpensesSplitter.WebApi/Migrations/20210110122853_AddDateTime.Designer.cs
+++ ExpensesSplitter.WebApi/Migrations/20210110122853_AddDateTime.Designer.cs
@@ -4,14 +4,16 @@ using ExpensesSplitter.WebApi.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ExpensesSplitter.WebApi.Migrations
 {
     [DbContext(typeof(ExpensesSplitterContext))]
-    partial class ExpensesSplitterContextModelSnapshot : ModelSnapshot
+    [Migration("20210110122853_AddDateTime")]
+    partial class AddDateTime
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
